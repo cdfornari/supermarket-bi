ce6581d5f7e4f1f86b842d620d7abddf3c1f2249
Alejandro <alejoguevarafm@gmail.com>
2023-10-29 17:55:30 -0400

    perf(app): :zap: Add parametro limit
    Se me paso agregar el parametro limit en la consulta xd

--- app/src/app/api/benefitsBranches/route.ts
+++ app/src/app/api/benefitsBranches/route.ts
@@ -2,17 +2,18 @@ import { pool } from "../../../../utils/database";
 
 interface RequestBody {
     sortOrder: boolean;
+    limit: number | null;
     branchFilter: string | null;
     date_start: string | null;
     date_end: string | null;
 }
 
 export async function POST(req: Request) {
-    const { sortOrder, branchFilter, date_start, date_end }: RequestBody = await req.json();
+    const { sortOrder, limit, branchFilter, date_start, date_end }: RequestBody = await req.json();
     const result = await pool.query(
         `SELECT * 
         FROM 
-        reportBranchesByBenefits(${sortOrder}, ${branchFilter}, ${date_start} , ${date_end})
+        reportBranchesByBenefits(${sortOrder}, ${limit} ,${branchFilter}, ${date_start} , ${date_end})
         `
     );
     return Response.json(result.rows);
--- app/src/app/api/popularProducts/route.ts
+++ app/src/app/api/popularProducts/route.ts
@@ -2,18 +2,19 @@ import { pool } from "../../../../utils/database";
 
 interface RequestBody {
     sortOrder: boolean;
+    limit: number | null;
     date_start: string | null;
     date_end: string | null;
     categoryFilter: string | null;
 }
 
 export async function POST(req: Request) {
-    const { sortOrder, date_start, date_end, categoryFilter }: RequestBody = await req.json();
+    const { sortOrder, limit, date_start, date_end, categoryFilter }: RequestBody = await req.json();
     console.log(sortOrder, date_start, date_end, categoryFilter);
     const result = await pool.query(
         `SELECT * 
         FROM 
-        reportProductsByPopularity(${sortOrder}, ${categoryFilter}, ${date_start}, ${date_end})
+        reportProductsByPopularity(${sortOrder}, ${limit} ,${categoryFilter}, ${date_start}, ${date_end})
         `
     );
     return Response.json(result.rows);
