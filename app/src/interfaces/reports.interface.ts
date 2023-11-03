4957c2656c8b2d7d2ed2a061d7323cc8b7ed2de6
Alejandro <alejoguevarafm@gmail.com>
2023-11-02 21:48:54 -0400

    feat(app): :sparkles: Interfaces
    Se realizaron las interfaces para los get data y para 3 reportes sino me pelo

---
+++ app/src/interfaces/reports.interface.ts
@@ -0,0 +1,21 @@
+export interface PopularProducts {
+    product_name: string;
+    product_description: string;
+    product_price: string;
+    product_category: string;
+    product_quantity: string;
+}
+
+export interface BenefitsBranches{
+    branch_name: string;
+    branch_address: string;
+    branch_phone: string;
+    branch_email: string;
+    branch_benefit: string;
+}
+
+export interface MoreAbsences{
+    first_name: string;
+    last_name: string;
+    quantity: number;
+}
