c79002c15a1664ae195e4229a80c3493bf389dad
Alejandro <alejoguevarafm@gmail.com>
2023-11-02 21:51:19 -0400

    refactor(app): :zap: Se realizo un cambio en base a un cambio que se hizo en la función

--- app/src/app/api/popularProducts/route.ts
+++ app/src/app/api/popularProducts/route.ts
@@ -3,18 +3,19 @@ import { pool } from "../../../utils/database";
 interface RequestBody {
     sortOrder: boolean;
     limit: number | null;
+    branch: number | null;
     date_start: string | null;
     date_end: string | null;
     categoryFilter: string | null;
 }
 
 export async function POST(req: Request) {
-    const { sortOrder, limit, date_start, date_end, categoryFilter }: RequestBody = await req.json();
+    const { sortOrder, limit, branch ,date_start, date_end, categoryFilter }: RequestBody = await req.json();
     console.log(sortOrder, date_start, date_end, categoryFilter);
     const result = await pool.query(
         `SELECT * 
         FROM 
-        reportProductsByPopularity(${sortOrder}, ${limit} ,${categoryFilter}, ${date_start}, ${date_end})
+        reportProductsByPopularity(${sortOrder}, ${branch} ,${limit} ,${categoryFilter}, ${date_start}, ${date_end})
         `
     );
     return Response.json(result.rows);
