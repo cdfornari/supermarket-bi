66e0f71203c00ea6ab818e5dde6243ca3b2261a7
Gianfranco <gianlanza10@gmail.com>
2023-11-03 00:58:25 -0400

    feat: :truck: renombre del reporte

---
+++ app/src/app/api/salesByTime/route.ts
@@ -0,0 +1,21 @@
+import { pool } from "../../../utils/database";
+
+
+interface RequestBody {
+    period : string | null,
+	categoryFilter : string | null,
+	branchFilter : string | null,
+	productNameFilter : string | null,
+	dateStart : string,
+	dateEnd : string
+}
+
+export async function POST(req: Request) {
+    const {period = "'month'", categoryFilter = null, branchFilter = null, productNameFilter = null, dateStart = "'2000-01-01'", dateEnd = "'2023-12-31'"}: RequestBody = await req.json();
+    const result = await pool.query(
+            `
+            SELECT * FROM reportSalesByTime(${period}, ${categoryFilter}, ${branchFilter}, ${productNameFilter}, ${dateStart}, ${dateEnd})
+        `
+    );
+    return Response.json(result.rows);
+}
